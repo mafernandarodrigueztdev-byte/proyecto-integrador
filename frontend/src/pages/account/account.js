584580efb49057e6bc703351861686fecb20ac3a
Tonantzin Alquisira <tonantzin.ap@gmail.com>
2026-06-19 13:10:53 -0600

    Se añadió formulario de registro

---
+++ frontend/src/pages/account/account.js
@@ -0,0 +1,17 @@
+const nombre = document.getElementById("nombre")
+const nombre = document.getElementById("apellidos")
+const nombre = document.getElementById("phone")
+const nombre = document.getElementById("email")
+const nombre = document.getElementById("emailconf")
+const nombre = document.getElementById("password")
+const nombre = document.getElementById("passwordconf")
+const parrafo = document.getElementById("warnings")
+
+form-register.addEventListener("submit", e=>{
+    e.preventDefault()
+    if(phone)
+}
+
+
+
+)
